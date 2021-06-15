6912a3fbaa22f38de6a14cf56d07e822e213cf4f
Michael Easter <codetojoy@gmail.com>
2021-06-15 08:16:33 -0300

    removed card field

--- src/main/java/net/codetojoy/simple/entity/Result.java
+++ src/main/java/net/codetojoy/simple/entity/Result.java
@@ -8,14 +8,8 @@ public class Result {
     @JsonProperty("people")
     private List<Person> people;
 
-    @JsonProperty("card")
-    private int card;
-
     @JsonProperty("message")
     private String message;
-
-    public int getCard() { return card; }
-    public void setCard(int card) { this.card = card; }
 
     public String getMessage() { return message; }
     public void setMessage(String message) { this.message = message; }
